aa5a59ff0a86f8aeb6542d50b4eb8bb3fd3fdc7d
qiqiliu <563735209@qq.com>
2016-09-20 15:56:37 +0800

    完善我的主页一

--- js/mypage.js
+++ js/mypage.js
@@ -25,6 +25,15 @@ $(document).ready(function () {
     }, function () {
         t = setInterval("showAuto()", 3000);
     });
+
+
+    $("#gotop1").click(function(e) {
+    TweenMax.to(window, 1.5, {scrollTo:0, ease: Expo.easeInOut});
+    var huojian = new TimelineLite();
+    huojian.to("#gotop1", 1, {rotationY:720, scale:0.6, y:"+=40", ease:  Power4.easeOut})
+    .to("#gotop1", 1, {y:-1000, opacity:0, ease:  Power4.easeOut}, 0.6)
+    .to("#gotop1", 1, {y:0, rotationY:0, opacity:1, scale:1, ease: Expo.easeOut, clearProps: "all"}, "1.4");
+   });
 })
 function showAuto() {
     n = n >= (count - 1) ? 0 : ++n;
